abf4521593fa2406fb4dcdbc77d21dcf47a2a4d0
DiamondAhuja <ahujadiamond@gmail.com>
2023-11-22 15:35:25 -0500

    pinging mock server works and shows a response

--- src/leo-server-app/src/routes/ping.ts
+++ src/leo-server-app/src/routes/ping.ts
@@ -1,32 +1,20 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 
-const Ping = require("../models/ping");
 const execAsync = promisify(exec);
 const express = require("express");
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("/ping", (req: any, res: any) => {
-  const ipAddress = "192.168.2.247"; 
-  execAsync(`ping -c 4 ${ipAddress}`)
+router.get("", (req: any, res: any) => {
+  const ipAddress = "192.168.2.247";
+  execAsync(`ping ${ipAddress}`)
     .then(({ stdout }) => {
-      const pingResult = new Ping({
-        ipAddress,
-        pingOutput: stdout,
-        success: true,
-      });
-
       res.json({ output: stdout });
     })
     .catch((error) => {
       console.error("Error:", error);
-      const pingResult = new Ping({
-        ipAddress,
-        pingOutput: error.message,
-        success: false,
-      });
 
       res
         .status(500)
